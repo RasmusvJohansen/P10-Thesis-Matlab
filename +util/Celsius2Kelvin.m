0dd27cd6e1f6b0983346b966a972bfda2cbe4dfe
RasmusVJohansen <rasmusvestergaardjohansen@gmail.com>
2024-02-15 09:21:43 +0100

    Added util folder

---
+++ +util/Celsius2Kelvin.m
@@ -0,0 +1,6 @@
+function [T_kelvin] = Celsius2Kelvin(T_Celcius)
+%DEG2KELVIN Summary of this function goes here
+%   Detailed explanation goes here
+T_kelvin = T_Celcius +273.15
+end
+
---
+++ +util/Kelvin2Celsius.m
@@ -0,0 +1,6 @@
+function [T_Celsius] = Kelvin2Celsius(T_Kelvin)
+%KELVIN2CELSIUS Summary of this function goes here
+%   Detailed explanation goes here
+T_Celsius = T_Kelvin-273.15;
+end
+
