afd658d66a3d162384f1dcc6c781e2b8049c0861
Mikkel <mikkel_26@fibermail.dk>
2024-02-20 10:29:33 +0100

    Added coupled model linearisation

--- +control/DesignProcedure1.m
+++ +control/DesignProcedure1.m
@@ -12,15 +12,15 @@ R = cell(1,param.n);
 for i=1:param.n
     Q{i} = sdpvar(3);
     y{i} = sdpvar(1,3,'full');
-
+    R{i} = sdpvar(3);
 end
-cali_Q = blkdiag(Q{:})
-Y = blkdiag(y{:})
+cali_Q = blkdiag(Q{:});
+Y = blkdiag(y{:});
+cali_R = blkdiag(R{:});
 %Lyapuanov
-constraints = [param.model.A*cali_Q+cali_Q*param.model.A.'+param.model.B_Bar*Y+Y.'*param.model.B_Bar.' <=0]
-constraints = [constraints, cali_Q >= 0]
-
-%Robust stability
+constraints = [param.model.A*cali_Q+cali_Q*param.model.A.'+param.model.B_Bar * inv(param.ctrl.Lambda_Bar)*Y + Y.'* inv(param.ctrl.Lambda_Bar)*param.model.B_Bar.' <=0];
+constraints = [constraints, cali_Q >= 0];
+constraints
 %constraints = []
 options = sdpsettings('verbose',0,'solver','mosek');
 sol = optimize(constraints,[],options);
@@ -32,6 +32,22 @@ Ks = zeros(1,3,param.n);
 for i=1:param.n
     Ks(:,:,i) = K(i,3*(i-1)+1:3*i);
 end
-Ks
+
+clear constraints
+tau = sdpvar(1);
+%Lyapuanov
+LPV = [cali_R * param.model.A + param.model.A.' * cali_R + tau*K.'*K, cali_R*param.model.B;param.model.B.'*cali_R, -tau*eye(4)]
+
+constraints = LPV <= 0;
+constraints = [constraints, cali_R >= 0, tau >=0];
+constraints
+
+%constraints = []
+options = sdpsettings('verbose',0,'solver','mosek');
+sol = optimize(constraints,[],options);
+check(constraints)
+double(tau)
+eig(double(cali_R))
+eig(param.model.A+param.model.B*K)
 end
 
--- +control/calculateOperatingPoint.m
+++ +control/calculateOperatingPoint.m
@@ -1,29 +1,43 @@
-function [T_wOP, q_OP, alpha, omega_s_OP, omega_OP] = calculateOperatingPoint(T_aOP,i,param)
+function param = calculateOperatingPoint(param)
 % Calculates the operating point for both the hydraulics, thermodynamics
 % and control parameters. First the water temperature is calculated, and
 % then used in the calculation of the flow at the operating point. Next the
 % flow is used to calculate the control parameter alpha and the two angular
 % velocity offset for the pumps at the linearisation point.
-
-    % Thermodynamic operating point
-    T_wOP = (-param.thermo.C_a*param.thermo.rho_a * param.ctrl.Q(i) * (param.thermo.T_A - T_aOP) + param.thermo.B(i)*T_aOP)/(param.thermo.B(i));
-    q_OP = (param.thermo.B(i)*(T_wOP - T_aOP))/(param.thermo.C_w*param.thermo.rho_w*(param.thermo.T_c - T_wOP));
     
-    % Hydraulic operating point
-        % Decoupled system, meaning only i'th flow is considered
-        alpha = sqrt((param.pump.a(i)+param.pipe.r(i)+param.pipe.R_c + 2*sum(param.pipe.R(1:i)))/param.pump.b(i));
-        omega_s_squared = (param.pump.a(1)+param.pipe.r(i))/param.pump.b(i) * param.ctrl.q(i)^2 + param.pipe.R_c/param.pump.b(i)*param.ctrl.q(i)^2;
-        for k=1:i  % Loops the pipe sections only considering the i'th pumps flow
-            omega_s_squared = omega_s_squared + 2*param.pipe.R(k)/param.pump.b(i)*param.ctrl.q(i)^2;
-        end
-        omega_s_OP = sqrt(omega_s_squared);
+    fprintf("Calculate Operating Point for T_a = %f [K]",param.ctrl.T_ref)
+    T_aOP = param.ctrl.T_ref;
+    for i=1:param.n
+        % Thermodynamic operating point
+        T_wOP = (-param.thermo.C_a*param.thermo.rho_a * param.ctrl.Q(i) * (param.thermo.T_A - T_aOP) + param.thermo.B(i)*T_aOP)/(param.thermo.B(i));
+        q_OP = (param.thermo.B(i)*(T_wOP - T_aOP))/(param.thermo.C_w*param.thermo.rho_w*(param.thermo.T_c - T_wOP));
+        
+        % Hydraulic operating point
+            % Decoupled system, meaning only i'th flow is considered
+            alpha = sqrt((param.pump.a(i)+param.pipe.r(i)+param.pipe.R_c + 2*sum(param.pipe.R(1:i)))/param.pump.b(i));
+            omega_s_squared = (param.pump.a(1)+param.pipe.r(i))/param.pump.b(i) * param.ctrl.q(i)^2 + param.pipe.R_c/param.pump.b(i)*param.ctrl.q(i)^2;
+            for k=1:i  % Loops the pipe sections only considering the i'th pumps flow
+                omega_s_squared = omega_s_squared + 2*param.pipe.R(k)/param.pump.b(i)*param.ctrl.q(i)^2;
+            end
+            omega_s_OP = sqrt(omega_s_squared);
+    
+            % Coupled system, meaning every flow is considered
+            omega_squared = (param.pump.a(1)+param.pipe.r(i))/param.pump.b(i) * param.ctrl.q(i)^2 + param.pipe.R_c/param.pump.b(i)*sum(param.ctrl.q)^2;
+            for k=1:i % Loops the pipe sections summing over the flows of the k'th and remaining pumps
+                omega_squared = omega_squared + 2*param.pipe.R(k)/param.pump.b(i)*sum(param.ctrl.q(k:end))^2;
+            end
+            omega_OP = sqrt(omega_squared);
 
-        % Coupled system, meaning every flow is considered
-        omega_squared = (param.pump.a(1)+param.pipe.r(i))/param.pump.b(i) * param.ctrl.q(i)^2 + param.pipe.R_c/param.pump.b(i)*sum(param.ctrl.q)^2;
-        for k=1:i % Loops the pipe sections summing over the flows of the k'th and remaining pumps
-            omega_squared = omega_squared + 2*param.pipe.R(k)/param.pump.b(i)*sum(param.ctrl.q(k:end))^2;
-        end
-        omega_OP = sqrt(omega_squared);
+
+        param.ctrl.alpha(i) = alpha;
+        param.ctrl.T_wOP(i) = T_wOP;
+        param.ctrl.T_aOP(i) = T_aOP;
+        param.ctrl.omega_OP(i) = omega_OP;
+        param.ctrl.omega_s_OP(i) = omega_s_OP;
+        param.ctrl.q_OP(i) = q_OP;
+        param.ctrl.xi_OP(3*(i-1)+1:3*i,1) = [T_wOP; param.ctrl.T_aOP(i); 0];
+    end
+    param.ctrl.Lambda_Bar = diag(alpha);
 
 end
 
