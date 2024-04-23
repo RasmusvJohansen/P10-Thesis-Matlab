e436bedf0873de8a89c40da3e3746460a1a68834
Mikkel <mikkel_26@fibermail.dk>
2024-04-23 08:41:06 +0200

    Updated explanations

--- +model/calculateMw.m
+++ +model/calculateMw.m
@@ -1,7 +1,7 @@
 function [Mw] = calculateMw(param)
 %CALCULATEMW Summary of this function goes here
 %   Detailed explanation goes here
-s = tf('s')
+s = tf('s');
 Am = [param.model.A+param.model.B*param.ctrl.K*param.model.Cy, zeros(12,4);
       param.model.Bwd*param.ctrl.K*param.model.Cy, param.model.Awd];
 
